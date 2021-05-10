86c8886a71f9091cbd1bf3cab32abe5962382eaa
Franco Carini <carini.franco@gmail.com>
2021-05-10 14:15:11 -0300

    Initial commit

---
+++ components/Layout.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Header from './Header'
+import Footer from './Footer'
+
+export default function Layout({title, keywords, description, children}) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name='description' content={description} />
+        <meta name='keywords' content={keywords} />
+      </Head>
+      <Header />
+      <div>
+        {children}
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+Layout.defaultProps = {
+  title: 'Sporthouse | Find the best products',
+  description: 'Find the best products online',
+  keywords: 'shoes, tshirts, sports'
+}
---
+++ pages/products/index.js
@@ -0,0 +1,9 @@
+import Layout from '@/components/Layout'
+
+export default function ProductsPage() {
+  return (
+    <Layout title='Products'>
+      
+    </Layout>
+  )
+}
